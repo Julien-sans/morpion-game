59ec42c7576cdac56ea41d9166b9e284b24c3e4c
Julien SANS <42937081+Julien-sans@users.noreply.github.com>
2019-05-12 15:16:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-let joueur1 = prompt('Bonjour Joueur 1, Quel est votre nom?');
-let joueur2 = prompt('Et vous joueur 2, quel est votre nom?');
+// Choix du nom des joueurs
+
+const myPlayer = (playerId) => {
+    let player = prompt('Bonjour Joueur 1, Quel est votre nom?');
+    if(player != null){
+        document.getElementById(playerId).innerHTML = player;
+        console.log(player)
+    };
+}
+
+window.onload = function() {
+   myPlayer('firstPlayer');
+   myPlayer('secondPlayer');
+}
